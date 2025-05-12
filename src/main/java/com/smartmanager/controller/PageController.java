dd648164feaf65b43cda73f669ed104ebf1ac933
abhishek-kumar-16 <kumarabhishekk16dec@gmail.com>
2025-05-12 23:35:40 +0530

    completed base layout and routing

--- src/main/java/com/smartmanager/controller/PageController.java
+++ src/main/java/com/smartmanager/controller/PageController.java
@@ -13,5 +13,32 @@ public class PageController {
     System.out.println("home page handler");
        return "home";
    }
+
+   @RequestMapping("about")
+   public String about() {
+       System.out.println("about page handler");
+       return "about";
+   }
+   @RequestMapping("services")
+   public String services() {
+       System.out.println("services page handler");
+       return "services";
+   }
+   @RequestMapping("contact")
+   public String contact() {
+       System.out.println("services page handler");
+       return "contact";
+   }
+   @RequestMapping("login")
+   public String login() {
+       System.out.println("services page handler");
+       return "login";
+   }
+   @RequestMapping("signup")
+   public String signup() {
+       System.out.println("services page handler");
+       return "signup";
+   }
+ 
    
 }
